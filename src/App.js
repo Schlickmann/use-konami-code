e44d19a77e76838ddad60a29722e36cd81424de5
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-04-20 18:44:03 -0700

    integration test

--- src/App.js
+++ src/App.js
@@ -1,1 +1,4 @@
-module.exports =  function sum(a,b) { return a + b };
+module.exports =  {
+  sum: function (a,b) { return a + b },
+  total: function (shipping, subTotal) { return "$" + this.sum(shipping, subTotal) }
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-const sum = require('./App');
+const functions = require('./App');
+
+// Fake function simulating a summation function
+const sum = jest.fn((a,b) => a + b);
 
 describe('First test suite', () => {
   it('should sum 5 + 2 and return 7', () => {
     const value = sum(5,2);
 
     expect(value).toBe(7);
+    expect(sum).toHaveBeenCalledTimes(1);
+    expect(sum).toHaveBeenCalledWith(5, 2);
+  });
+
+  it('should return a string with a dolar sign plus shipping and subtotal added together', () => {
+    expect(functions.total(5, 43)).toBe("$48");
   });
 });
