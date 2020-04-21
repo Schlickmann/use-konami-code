362cb1b6715ea1018530d9e1f82847d5ed4d134d
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-04-20 18:59:47 -0700

    new project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
-module.exports =  {
-  sum: function (a,b) { return a + b },
-  total: function (shipping, subTotal) { return "$" + this.sum(shipping, subTotal) }
-};
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,9 @@
-const functions = require('./App');
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-// Fake function simulating a summation function
-const sum = jest.fn((a,b) => a + b);
-
-describe('First test suite', () => {
-  it('should sum 5 + 2 and return 7', () => {
-    const value = sum(5,2);
-
-    expect(value).toBe(7);
-    expect(sum).toHaveBeenCalledTimes(1);
-    expect(sum).toHaveBeenCalledWith(5, 2);
-  });
-
-  it('should return a string with a dolar sign plus shipping and subtotal added together', () => {
-    expect(functions.total(5, 43)).toBe("$48");
-  });
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
